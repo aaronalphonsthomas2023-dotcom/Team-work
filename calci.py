042da4cad1ccd7b7861fcd13d4fc31ca8453f804
Maheshwar23 <marvelmagi23699@gmail.com>
2025-12-18 15:14:40 +0530

    Update calci.py

--- calci.py
+++ calci.py
@@ -4,8 +4,8 @@ def subtract(a,b):
     return a-b
 print("Enter 1 or 2")
 choice=int(input("Enter Choice"))
-a=float(input("Enter 1st number"))
-b=float(input("Enter 2nd number"))
+a=float(input("Enter first number"))
+b=float(input("Enter second number"))
 if choice==1:
     print(add(a,b))
 else:
